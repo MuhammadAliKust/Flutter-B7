52dcb79f5bda752562a31ce957f6d5af87b6846c
Muhammad Ali Nizami <kustalics@gmail.com>
2024-10-10 12:02:13 +0500

    Padding Done

--- lib/login.dart
+++ lib/login.dart
@@ -9,28 +9,37 @@ class LoginView extends StatelessWidget {
       appBar: AppBar(
         title: Text("Login"),
       ),
-      body: Column(
-        children: [
-          TextField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-                label: Text("Email"),
-                hintText: 'test@gmail.com',
-                prefixIcon: Icon(Icons.email),
-                suffixIcon: Icon(Icons.email)),
-          ),
-          TextField(
-            keyboardType: TextInputType.emailAddress,
-            // obscureText: true,
-            decoration: InputDecoration(
-                label: Text("Password"),
-                hintText: 'Password must be 6 or more characters',
-                prefixIcon: Icon(Icons.lock),
-                suffixIcon: Icon(Icons.visibility)),
-          ),
-          SizedBox(height: 30,),
-          ElevatedButton(onPressed: (){}, child: Text("Login"))
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 30),
+
+        child: Column(
+          children: [
+            Text("First Text"),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  label: Text("Email"),
+                  hintText: 'test@gmail.com',
+                  prefixIcon: Icon(Icons.email),
+                  suffixIcon: Icon(Icons.email)),
+            ),
+
+            Text("First Text"),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              // obscureText: true,
+              decoration: InputDecoration(
+                  label: Text("Password"),
+                  hintText: 'Password must be 6 or more characters',
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: Icon(Icons.visibility)),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(onPressed: () {}, child: Text("Login"))
+          ],
+        ),
       ),
     );
   }
