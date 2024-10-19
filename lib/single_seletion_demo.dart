0afb69cbe6adb75e0538e1fad50fdde17de97846
Muhammad Ali Nizami <kustalics@gmail.com>
2024-10-19 11:41:32 +0500

    Multiple Selection Done

--- lib/single_seletion_demo.dart
+++ lib/single_seletion_demo.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class SingleSeletionDemo extends StatefulWidget {
@@ -8,7 +10,7 @@ class SingleSeletionDemo extends StatefulWidget {
 }
 
 class _SingleSeletionDemoState extends State<SingleSeletionDemo> {
-  int selectedIndex = -1;
+  List<int> selectedIndex = [];
 
   @override
   Widget build(BuildContext context) {
@@ -16,21 +18,23 @@ class _SingleSeletionDemoState extends State<SingleSeletionDemo> {
       appBar: AppBar(
         title: Text("Single Selection Demo"),
       ),
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, i) {
-            return ListTile(
-              onTap: () {
-                selectedIndex = i;
-                setState(() {});
-              },
-              tileColor: selectedIndex == i ? Colors.blue : Colors.white,
-              leading: Icon(Icons.notifications),
-              title: Text("Selected Index: $selectedIndex"),
-              subtitle: Text("Loop Index: $i"),
-              trailing: Icon(Icons.arrow_forward_ios),
-            );
-          }),
+      body: ListView.builder(itemBuilder: (context, i) {
+        return ListTile(
+          onTap: () {
+            if (selectedIndex.contains(i)) {
+              selectedIndex.remove(i);
+            } else {
+              selectedIndex.add(i);
+            }
+
+            setState(() {});
+          },
+          tileColor: selectedIndex.contains(i) ? Colors.blue : Colors.white,
+          leading: Icon(Icons.notifications),
+          title: Text("Selected Index: ${selectedIndex.toString()}"),
+          subtitle: Text("Loop Index: $i"),
+        );
+      }),
     );
   }
 }
